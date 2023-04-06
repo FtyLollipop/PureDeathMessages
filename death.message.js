432126390eb06c51287d8956164620002ce63347
FtyLollipop <ftylollipop@outlook.com>
2023-04-06 17:28:53 +0800

    Update death.message.js

--- death.message.js
+++ death.message.js
@@ -1,4 +1,4 @@
-let config = new JsonConfigFile('plugins/death.message/config.json', {'groups': [], 'edition': 'java'})
+let config = new JsonConfigFile('plugins/death.message/config.json', '{"groups": [], "edition": "java"}')
 let entityData = (new JsonConfigFile(`plugins/death.message/resources/entity.json`)).get(config.get('edition'))
 let messageData = (new JsonConfigFile(`plugins/death.message/resources/message.json`)).get(config.get('edition'))
 let mapData = (new JsonConfigFile('plugins/death.message/resources/map.json')).get("map")
