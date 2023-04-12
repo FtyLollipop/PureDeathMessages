db06dee5049e1da6188730750d935c48ca4925ac
FtyLollipop <ftylollipop@outlook.com>
2023-04-12 10:54:48 +0800

    Update death.message.js

--- death.message.js
+++ death.message.js
@@ -46,6 +46,7 @@ function isTamed(mob) {
 }
 
 function deathEventHandler(mob, source, cause, entity, message, map, config) {
+    if(mc.runcmdEx('gamerule showdeathmessages').output.match(/true|false/) === 'false') { return }
     const enabledEntity = config?.enabledEntity ?? {
         "minecraft:cat": true,
         "minecraft:donkey": true,
